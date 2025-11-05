71d61825bb1e72f2d09dcdd18da29c76a856694d
Akash Mahato <akashmahato@Akashs-MacBook-Air.local>
2025-11-05 21:51:29 +0530

    services page

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,7 +15,7 @@ const navLinks = [
   { name: 'Home', href: '/' },
   { name: 'About', href: '/about' },
   { name: 'Projects', href: '/projects' },
-  { name: 'Services', href: '/ services' },
+  { name: 'Services', href: '/services' },
   { name: 'Portfolio', href: '/social-portfolio' },
 ];
 
